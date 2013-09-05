521ee5129ef7611de60c04c4d964e1c1ea16f68b
jsw <jsw@3b1c9cea-dd9d-4dfb-bcb5-e65b7d4966e2>
2013-09-05 05:45:31 +0000

    Added comments to Permutation.

--- src/statelet/permutation/Permutation.java
+++ src/statelet/permutation/Permutation.java
@@ -28,12 +28,27 @@ import java.util.Set;
 
 public interface Permutation 
 {
+	/**
+	 * @return all values that that are non-identically mapped by this permutation
+	 */
+	
 	public Set< Integer > preimage();
 	
+	/**
+	 * @return smallest value that is non-identically mapped by this permutation
+	 */
 	public int minPreimage();
+
+	/**
+	 * @return largest value that is non-identically mapped by this permutation
+	 */
 	public int maxPreimage();
+
+	/**
+	 * @return mapping of value under by this permutation
+	 */
 	
-	public int image( int point );
+	public int image( int value );
 }
 
 // End ///////////////////////////////////////////////////////////////
